8b3d3aee27ddfca561c2433e4356034fa8b1acc3
agabin <agabin@appdev.yearup.org>
2024-09-25 15:33:37 -0400

    completed ShoppingList app

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping list:");
+        System.out.println("-------------------");
+        System.out.println("Apples");
+        System.out.println("Olives");
+    }
 }
